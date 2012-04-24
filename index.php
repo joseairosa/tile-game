b4fba45817d15932df46893f332d9c6bb82287d5
José Airosa <j.airosa@digital-science.com>
2012-04-24 16:26:29 +0100

    Changed configuration for mobile devices

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ $app_name = idx($app_info, 'name', '');
 <html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
     <title><?php echo he($app_name); ?></title>
     <link rel="stylesheet" href="stylesheets/screen.css" media="Screen" type="text/css" />
